2a07eba99041b500c800bfb2b9cd3d41ba5d93d2
Lushogv <luciano20746@gmail.com>
2023-02-04 00:59:59 -0300

    fix cors 1

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ const app = express();
 config();
 const PORT = process.env.PORT | 3001;
 
+var whitelist = ['http://localhost:5173']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  }else{
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
 app.use(express.json());
-app.use(cors());
-app.get("/", (req, res) => res.send("Hello world"));
-app.post("/prueba", (req, res) => {
+// app.use(cors());
+app.get("/", cors(corsOptionsDelegate),(req, res) => res.send("Hello world"));
+app.post("/prueba",cors(corsOptionsDelegate), (req, res) => {
   const { prueba } = req.body;
   res.status(201).send(prueba);
 });
