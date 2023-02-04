3b84f211827054bb66eaab34c52492bb9f50abc3
Lushogv <luciano20746@gmail.com>
2023-02-04 01:32:13 -0300

    fix cors 7

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ import { config } from "dotenv";
 const app = express();
 config();
 const PORT = process.env.PORT | 3001;
-
+const whiteList = ["http://127.0.0.1:5173","http://127.0.0.1:3000","http://localhost:5173"]
 app.use(cors({
-  origin: "*"
+  origin: function(origin, callback){
+      if(whiteList.includes(origin)){
+          return callback(null, origin)
+      }
+      return callback("Error de CORS origin: "+ origin+ " No autorizado")
+  },
+  // credentials: true
 }));
 app.use(express.json());
 
